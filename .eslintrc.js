a4d2bc1ae7c435db262c85d50788226f2c524e30
Vitor Silva <developer.vitorsilva@gmail.com>
2021-08-01 21:34:19 -0300

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-console': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+};
---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+
+class App {
+  public express: express.Application;
+
+  public constructor() {
+    this.express = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  private middlewares(): void {
+    this.express.use(express.json());
+  }
+
+  private routes(): void {
+    this.express.get('/', (req, res) => res.send('hello'));
+  }
+}
+
+export default new App().express;
