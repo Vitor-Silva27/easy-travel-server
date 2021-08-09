871190a7b0e26c79676b1b078b93e08ad5a62fb4
Vitor Silva <developer.vitorsilva@gmail.com>
2021-08-09 10:31:45 -0300

    created agency and trip entities

---
+++ src/app/Agency/AgencyEntity.ts
@@ -0,0 +1,8 @@
+/* eslint-disable camelcase */
+interface Agency {
+    agency_id?: string;
+    agency_name: string;
+    email:string;
+    password:string;
+}
+export default Agency;
---
+++ src/app/Trip/TripEntity.ts
@@ -0,0 +1,9 @@
+/* eslint-disable camelcase */
+interface Trip {
+    id?: string;
+    name: string;
+    description: string;
+    value: number;
+    owner_agency:string;
+}
+export default Trip;
